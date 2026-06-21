34290ee7f6f644512395fefec1fd5289bb306143
Nilsix <a.nilsix20@gmail.com>
2026-06-21 13:37:58 +0200

    0.2

--- FileInjector/Inject.bat
+++ FileInjector/Inject.bat
@@ -6,8 +6,7 @@ python fsv2csv.py to_fsv CommonParam.csv
 move "CharaStatus.fsv" "..\Files\Bleach Rebirth of Souls Community Patch\Script\"
 move "CommonParam.fsv" "..\Files\Bleach Rebirth of Souls Community Patch\Script\"
 
-pause
+
 
 ConvertToCSV
 
-pause
