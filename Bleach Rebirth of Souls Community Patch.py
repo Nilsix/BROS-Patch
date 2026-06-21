ed7972bfef963bf865ce5284dbcd564e00db6467
Nilsix <a.nilsix20@gmail.com>
2026-06-21 14:41:31 +0200

    music fix

--- Bleach Rebirth of Souls Community Patch.py
+++ Bleach Rebirth of Souls Community Patch.py
@@ -16,7 +16,7 @@ from pathlib import Path
 try: 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     window = Tk()
-    winsound.PlaySound(os.path.join(BASE_DIR,"ressources","FaintGlow.wav"),winsound.SND_FILENAME)
+    winsound.PlaySound(os.path.join(BASE_DIR,"ressources","FaintGlow.wav"),winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP)
     ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)  
     window.title("Bleach Community Patch")
     window.geometry("1080x800")
