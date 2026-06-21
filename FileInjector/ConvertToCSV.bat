d9724eb2273d6943cb2fc386f86e5f56682c9187
Nilsix <a.nilsix20@gmail.com>
2026-06-21 17:38:46 +0200

    yellow fs nerf

--- FileInjector/ConvertToCSV.bat
+++ FileInjector/ConvertToCSV.bat
@@ -4,4 +4,3 @@ python fsv2csv.py to_csv "..\Files\Bleach Rebirth of Souls Community Patch\Scrip
 python fsv2csv.py to_csv "..\Files\Bleach Rebirth of Souls Community Patch\Script\CommonParam.fsv"
 move "..\Files\Bleach Rebirth of Souls Community Patch\Script\Charastatus.csv"
 move "..\Files\Bleach Rebirth of Souls Community Patch\Script\CommonParam.csv"
-pause
