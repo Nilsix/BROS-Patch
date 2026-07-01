856bc53723a185030ec61d1e79d08ba05649a79b
Nilsix <a.nilsix20@gmail.com>
2026-07-01 11:34:36 +0200

    m

--- Bleach Rebirth of Souls Community Patch.py
+++ Bleach Rebirth of Souls Community Patch.py
@@ -82,7 +82,8 @@ try:
     
     window = Tk()
     try:
-        ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)  
+        pass
+        #ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)  
     except:
         pass
     window.title("Bleach Community Patch")
